c3c20e715093e394cb9e0ac067091a7ba93ced7c
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2026-07-20 20:49:19 +0300

    finished the car game

--- car_engine.py
+++ car_engine.py
@@ -1,3 +1,24 @@
 #enter start to start engine
 #enter stop to stop engine
 #enter quit to exit
+
+engine_status = input("Enter keyword: ")
+engine_status.lower()
+
+while engine_status != "quit":
+    if engine_status == "start":
+        print("Car started")
+        break
+    elif engine_status == "stop":
+        print("Cart stopped")
+        break
+    elif engine_status == "help":
+        print("""
+Start - to start the car
+Stop - to stop the car
+Quit - to exit
+        """)
+        break
+    else:
+        print("You entered wrong command")
+        break

47e97d78bebf34cadcb83290a95f687b29b0744f
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2026-07-21 22:10:22 +0300

    some tough challenge on nested loop

---
+++ letter_F_challenge.py
@@ -0,0 +1,14 @@
+numbers = [5, 2, 5, 2, 2]
+lateral_numbers = [2, 2, 2, 2, 5]
+
+for number in numbers:
+    output = ""
+    for count in range (number):
+        output += "x"
+    print(output)
+
+for lateral in lateral_numbers:
+    outcome = ""
+    for come in range(lateral):
+        outcome += "x"
+    print(outcome)

299645c0b1361ac9c664e53d81291c23e797cd7b
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2026-07-21 21:20:56 +0300

    modified to detect repeated commands

--- car_commands.py
+++ car_commands.py
@@ -1,11 +1,20 @@
 command = ""
+started = False
 
 while True:
     command = input("> ").lower()
     if command == "start":
-        print("Car started...")
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car started...")
     elif command == "stop":
-        print("Car stopped...")
+        if not started:
+            print("Car already stopped")
+        else:
+            started = False
+            print("Car stopped...")
     elif command == "help":
         print("""
 Start - to start car

b1c0a203b2287ad338a99c6a642e544af947a4c7
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2026-08-14 22:39:08 +0300

    added parameters and return statement

--- functions.py
+++ functions.py
@@ -1,8 +1,14 @@
 length = 10
 width = 5
 
-def area_rectangle ():
+def area_rectangle():
     area = length * width
     print(area)
 
+def area_square(side):
+    print(side**2)
+    return side**2
+
+
+area_square(4)
 area_rectangle()
